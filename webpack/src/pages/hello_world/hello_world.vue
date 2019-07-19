8ede1055d6f84f3462dbb8066c32589c71ac4af8
EugeneJao <Eugene_jao@icloud.com>
2019-07-19 08:35:23 +0800

    完善webpack功能展示demo

---
+++ webpack/src/pages/hello_world/hello_world.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>hello world!</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'hello-world',
+};
+</script>
+<style lang="less">
+  h1 {
+    background-color: lightblue;
+  }
+</style>
+
